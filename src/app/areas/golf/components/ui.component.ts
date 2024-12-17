038d89a02f88fe773fdaa20e8e269d233be51687
Jeffry Gonzalez <jeff@hypertheory.com>
2024-12-17 14:48:54 -0500

    starting lab

--- src/app/areas/golf/components/ui.component.ts
+++ src/app/areas/golf/components/ui.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  signal,
-  computed,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { GolfStore } from '../services/golf-store';
 
 @Component({
