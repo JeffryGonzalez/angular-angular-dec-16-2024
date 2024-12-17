1049510942a9aad30635369c39248d3fe0afce38
Jeffry Gonzalez <jeff@hypertheory.com>
2024-12-17 11:07:23 -0500

    before break

--- src/app/areas/news/components/news-list.component.ts
+++ src/app/areas/news/components/news-list.component.ts
@@ -2,6 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  resource,
   signal,
 } from '@angular/core';
 import { NewsArticle } from '../types';
@@ -20,7 +21,7 @@ import { NewsItemComponent } from './news-item/news-item.component';
       <p>You are really behind on your reading! Read some stuff.</p>
     }
     <section>
-      @for (article of articles(); track article.id) {
+      @for (article of articles.value(); track article.id) {
         <!-- display the app-news-item -->
         <app-news-item
           (linkRead)="readTheArticle($event)"
@@ -36,36 +37,13 @@ import { NewsItemComponent } from './news-item/news-item.component';
 export class NewsListComponent {
   // data in a component is a Signal, or .................................. an observable
   preferredHeader = signal('This is a header from a signal');
-  articles = signal<NewsArticle[]>([
-    // none of that change detection stuff I just talked has anything to do with signals.
-    {
-      id: '1',
-      title: 'Angular Inputs and Outputs',
-      shortDescription:
-        'Read about Angular inputs and outputs in "modern" Angular',
-      link: 'https://applied-angular.hypertheory.com/guides/components.html',
-      datePublished: '2024-12-16T17:32:12.556Z',
-      linkSlug: 'Read about Angular Inputs and Outputs',
-    },
-    {
-      id: '2',
-      title: 'Creating an Angular Application',
-      shortDescription: 'Learn how to create a super rad Angular app!',
-      link: 'https://applied-angular.hypertheory.com/guides/angular-setup.html',
-      datePublished: '2024-12-05T13:18:12.556Z',
-      linkSlug: 'Read about Creating an App',
-    },
-    {
-      id: '3',
-      title: 'Deborah Kurata YouTube',
-      shortDescription:
-        'Youtube Videos about Angular Signals and stuff from Deborah Kurata',
-      link: 'https://www.youtube.com/@deborah_kurata/videos',
-      datePublished: '2024-12-16T21:20:17.014Z',
-      linkSlug: 'Deborah Kurata Videos',
-    },
-  ]);
 
+  articles = resource<NewsArticle[], unknown>({
+    loader: () =>
+      fetch('http://fake-news.some-fake-server.com/news-feed').then((r) =>
+        r.json(),
+      ),
+  });
   readArticles = signal<NewsArticle[]>([]);
   readArticleCount = computed(() => this.readArticles().length);
   readTheArticle(article: NewsArticle) {
