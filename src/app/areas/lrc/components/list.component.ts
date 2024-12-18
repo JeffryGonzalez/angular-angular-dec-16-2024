c1abebe00f0b90d573dfb7e2dcab37d60fae4f0c
Jeffry Gonzalez <jeff@hypertheory.com>
2024-12-18 14:13:52 -0500

    layout

---
+++ src/app/areas/lrc/components/list.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-lrc-list',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ` <p>List Goes Here</p> `,
+  styles: ``,
+})
+export class ListComponent {}
--- src/app/areas/lrc/lrc.component.ts
+++ src/app/areas/lrc/lrc.component.ts
@@ -1,10 +1,18 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ListComponent } from './components/list.component';
+import { EntryComponent } from './components/entry.component';
 
 @Component({
   selector: 'app-lrc',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: ` <p>LRC Stuff Goes Here</p> `,
+  imports: [ListComponent, EntryComponent],
+  template: `
+    <p>LRC Stuff Goes Here</p>
+    <div class="flex">
+      <app-lrc-list />
+      <app-lrc-entry />
+    </div>
+  `,
   styles: ``,
 })
 export class LrcComponent {}
