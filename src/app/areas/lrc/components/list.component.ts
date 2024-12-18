ce21426e3123bf2a868654f6b9718c545c07154e
Jeffry Gonzalez <jeff@hypertheory.com>
2024-12-18 16:11:20 -0500

    can list them out

--- src/app/areas/lrc/components/list.component.ts
+++ src/app/areas/lrc/components/list.component.ts
@@ -1,10 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { PostsStore } from '../services/post-store';
+import { DatePipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-lrc-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: ` <h1>List of Posts</h1> `,
+  imports: [DatePipe],
+  template: `
+    <h1>List of Posts</h1>
+    @for (post of store.posts(); track post.id) {
+      <div>
+        <p>Name: {{ post.name }}</p>
+        <p>Description: {{ post.description }}</p>
+        <p>Posted By: {{ post.postedBy }}</p>
+        <p>Posted On: {{ post.datePosted | date: 'medium' }}</p>
+        <p>Link: {{ post.link }}</p>
+      </div>
+    }
+  `,
   styles: ``,
 })
-export class ListComponent {}
+export class ListComponent {
+  store = inject(PostsStore);
+}
