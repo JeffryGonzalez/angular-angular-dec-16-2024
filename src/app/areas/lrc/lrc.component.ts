fe720c4114e951f81aba817adea75f976095f73d
Jeffry Gonzalez <jeff@hypertheory.com>
2024-12-18 12:36:24 -0500

    before lunch

---
+++ src/app/areas/lrc/lrc.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-lrc',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ` <p>LRC Stuff Goes Here</p> `,
+  styles: ``,
+})
+export class LrcComponent {}
